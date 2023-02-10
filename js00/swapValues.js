c69fa8ca11c17cbc264028eae36fbf0bb426bbae
Abdoul-99 <124527439+Abdoul-99@users.noreply.github.com>
2023-02-10 22:10:53 +0100

    Update swapValues.js

--- js00/swapValues.js
+++ js00/swapValues.js
@@ -2,10 +2,11 @@ var a = 10;
 
 var b =20;
 
-var temp;
-temp = a
-a = b;
+var c = 0
 
-b = temp
+c=a
+a=b
+b=c
 
-console.log(a,b)
+console.log("New value of a":,a)
+Console.log("New value of b":,b)
